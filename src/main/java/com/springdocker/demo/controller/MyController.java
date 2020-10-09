054a2b293a902361114008fc998b0a702a4f90d9
Pathik <pathik.parmar@gmail.com>
2020-10-09 19:37:24 -0400

    Added controller and modified port Ready for prod.

---
+++ src/main/java/com/springdocker/demo/controller/MyController.java
@@ -0,0 +1,16 @@
+package com.springdocker.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app")
+public class MyController {
+
+    @GetMapping
+    public String greetings(){
+        return "Hello User";
+    }
+
+}
